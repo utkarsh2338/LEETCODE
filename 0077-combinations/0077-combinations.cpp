9136c9d13ec689ee8f042a760ed8bafa7e7b9346
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2024-12-27 02:29:13 +0530

    Time: 105 ms (21.87%), Space: 131.4 MB (16.95%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    void solve(int start,int n,int k,vector<int>&temp){
+        if(k==0){
+            result.push_back(temp);
+            return;
+        }
+        if(start>n){
+            return;
+        }
+        temp.push_back(start);
+        solve(start+1,n,k-1,temp);
+        temp.pop_back();
+        // nahi lene ke baaad bhi mujhe explore karna padega ye ek case bas dhyan rakhna tha
+        solve(start+1,n,k,temp);// k decrement tabhi hoga jab maine element liya ho
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>temp;
+        solve(1,n,k,temp);
+        return result;
+    }
+};
