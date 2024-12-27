56b811175612e63cd50df99baafffbde32732c8f
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2024-12-28 01:53:40 +0530

    Time: 0 ms (100%), Space: 7.9 MB (18.3%) - LeetHub

---
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -0,0 +1,22 @@
+#define mod 1000000007 // Note: Fixed the mod value to 10^9 + 7
+class Solution {
+public:
+    long long power(long long x, long long y) {
+        if (y == 0) return 1;
+        long long ans = power(x, y / 2);
+        ans *= ans;
+        ans %= mod;
+        if (y % 2) {
+            ans *= x;
+            ans %= mod;
+        }
+        return ans;
+    }
+
+    int countGoodNumbers(long long n) {
+        long long odd = n / 2;            // Count of odd positions
+        long long even = n / 2 + n % 2;  // Count of even positions
+        long long result = (power(5, even) * power(4, odd)) % mod;
+        return result;
+    }
+};
