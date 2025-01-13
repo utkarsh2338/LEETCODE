e3d69574141d03ab6a5528c27a357be74c742fe4
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-01-13 11:27:34 +0530

    Time: 28 ms (58.47%), Space: 32.1 MB (74.19%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int minimumLength(string s) {
+        // we will go with the first approach that is by counting the frequency of each characters
+        // the approach is simple count the frequency of each character and then if the frequency of any particular character goes 3 then remove the count of that frequency by 2 that is from the front and from the end;
         int n = s.length();
-        vector<int>hash(26,0);
-        // we will count the frequency of each characters and then according to the frequency we will do the operation
+        vector<int>hash(26,0);// we are not taking the map because there is only lower case alphabets in the string if it would have been both lowercase as well as upper case then we would have taken the map
         for(int i=0;i<n;i++){
-            hash[s[i] - 'a']++;
+            hash[s[i] -'a']++;
             if(hash[s[i] - 'a'] == 3){
-                hash[s[i] -'a'] -= 2;// deleted two characters
+                hash[s[i] - 'a'] -= 2; 
             }
         }
         int ans = 0;
         for(int i=0;i<26;i++){
-            if(hash[i] == 0) continue;
             ans += hash[i];
         }
-        // for example: if frequency of a is 3 then only we can remove one character from front and one from end
-
-    return ans;
+        return ans;
     }
 };
