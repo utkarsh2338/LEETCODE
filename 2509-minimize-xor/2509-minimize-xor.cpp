ab80adbfd472d34c4f25e4b57fe0901d469bb964
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-01-16 20:18:26 +0530

    Time: 0 ms (100%), Space: 8 MB (64.52%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.cpp
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
-    int minimizeXor(int num1, int num2) {
-        int result = 0;
-
-        int targetSetBitsCount = __builtin_popcount(num2);
-        int setBitsCount = 0;
-        int currentBit = 31; 
-        while (setBitsCount < targetSetBitsCount) {
-            if (isSet(num1, currentBit) ||
-                (targetSetBitsCount - setBitsCount > currentBit)) {
-                setBit(result, currentBit);
-                setBitsCount++;
-            }
-            currentBit--;  
-        }
-
-        return result;
+    bool isSet(int& x, int bit){
+        return x& (1<<bit);
     }
+    bool setBit(int& x,int bit){
+        return x |= (1<<bit);
+    }
+    bool unsetBit(int& x, int bit){
+        return x &= ~(1<<bit);
+    }
+    int minimizeXor(int num1, int num2) {
+        int x = num1;
+        int requiredSetBitCount = __builtin_popcount(num2);// no of set bits in num2
+        int currSetBitCount = __builtin_popcount(x);
 
-private:
-    // Helper function to check if the given bit position in x is set (1).
-    bool isSet(int x, int bit) { return x & (1 << bit); }
-
-    // Helper function to set the given bit position in x to 1.
-    void setBit(int &x, int bit) { x |= (1 << bit); }
+        int bit  = 0;// position of current bit 
+        if(currSetBitCount<requiredSetBitCount){
+            while(currSetBitCount<requiredSetBitCount){
+                if(!isSet(x,bit)){
+                    setBit(x,bit);
+                    currSetBitCount++;
+                }
+                bit++;
+            }
+        }
+        else if(currSetBitCount>requiredSetBitCount){
+            while(currSetBitCount>requiredSetBitCount){
+                if(isSet(x,bit)){
+                    unsetBit(x,bit);
+                    currSetBitCount--;
+                }
+                bit++;
+            }
+        }
+        return x;
+    }
 };
