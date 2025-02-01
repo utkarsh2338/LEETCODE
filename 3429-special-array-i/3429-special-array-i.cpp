4239cacb83112a6ef03c61dafd6aed25272a95ae
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-02-01 09:13:58 +0530

    Time: 0 ms (100%), Space: 27.7 MB (25%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -3,7 +3,10 @@ public:
     bool isArraySpecial(vector<int>& nums) {
         int n = nums.size();
         for(int i=0;i<n-1;i++){
-            if((nums[i] + nums[i+1]) %2 == 0){
+            if((nums[i] % 2 == 0) && (nums[i+1] %2 == 0)){
+                return false;
+            }
+            else if((nums[i] % 2 != 0 ) && (nums[i+1] % 2 != 0)){
                 return false;
             }
         }
