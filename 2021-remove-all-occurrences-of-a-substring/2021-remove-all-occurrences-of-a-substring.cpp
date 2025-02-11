d2493d6c6c44c7f8fa8d247ba57db997d09a9e8a
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-02-12 00:34:47 +0530

    Time: 0 ms (100%), Space: 9.5 MB (34.81%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -6,7 +6,7 @@ public:
         while(true){
             int idx = s.find(part);
             
-            if(idx == string:: npos){
+            if(idx == string:: npos){// agr nahi milta jo mai dhundh raha toh fir mai break kar jaunga
                 break;
             }
             s.erase(idx,part.length());//kis index se start karenge aur kitna hatayenge vo dena hota hai
