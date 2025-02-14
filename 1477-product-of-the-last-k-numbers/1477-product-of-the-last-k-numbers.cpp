02ed7b5003ed28996c8a2dfee759b4cf5666cec9
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-02-15 00:55:27 +0530

    Time: 823 ms (28.37%), Space: 78.2 MB (43.27%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,14 +1,15 @@
 class ProductOfNumbers {
 public:
+// BRUTE FORCE APPROACH
     vector<int> stream;
     void add(int num) {
-        stream.push_back(num);
+        stream.push_back(num);// 0(1) complexity isme lag rahi hai
     }
     
     int getProduct(int k) {
         int prod = 1;
         int n = stream.size();
-        for(int i = n-1;i>n-k-1;i--){
+        for(int i = n-1;i>n-k-1;i--){// isme 0(n) lag rahi hai
             prod *= stream[i];
         }
         return prod;
