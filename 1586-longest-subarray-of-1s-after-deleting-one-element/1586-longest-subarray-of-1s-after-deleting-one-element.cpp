0a76702a750d171b0294c43dbc2ecf4edcd1f253
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-03-18 18:11:56 +0530

    Time: 5 ms (10.07%), Space: 60.1 MB (60.43%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int i = 0, j = 0;
+        int i =0,j =0;
+        int cnt = 0,ans = 0;
         int n = nums.size();
-        int cnt = 0; // Count of zeros
-        int ans = 0;
 
-        while (j < n) {
-            if (nums[j] == 0) {
-                cnt++;
+        while(j<n){
+            if(nums[j] == 0){
+                cnt ++;
             }
-
-            if (cnt > 1) { // More than one zero is not allowed
-                if (nums[i] == 0)
-                    cnt--;
+            if(cnt > 1){
+                if(nums[i] == 0){
+                    cnt --;
+                }
                 i++;
             }
-
-            ans = max(ans, j - i); // Maximum valid subarray length
+            ans = max(ans,j-i);
             j++;
         }
-
         return ans;
     }
 };
