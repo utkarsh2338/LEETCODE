be9d0d7541e26c6e4a4e23a1b24e0df355745432
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-03-19 07:56:51 +0530

    Time: 19 ms (8.79%), Space: 140 MB (47.55%) - LeetHub

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    /*
+    for each index i, we check whether nums[i - 2] is still 0. If it is, then we
+    must flip the triplet ending at i (nums[i - 2], nums[i - 1], nums[i]).
+    */
+    int minOperations(vector<int>& nums) { 
+        int count = 0;
+        int n = nums.size(); 
+        for(int i=2;i<n;i++){
+            if(nums[i-2] == 0){
+                count++;
+                // flip using xor
+                nums[i-2] ^= 1;
+                nums[i-1] ^= 1;
+                nums[i] ^= 1;
+            }
+        }
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(sum == nums.size()) return count;
+        return -1;
+        }
+};
