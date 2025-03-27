b5d2dbc83c978596187af3857a156a959d36787a
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-03-27 06:40:19 +0530

    Time: 39 ms (49.59%), Space: 98.7 MB (43.44%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int n = nums.size();
+        int dominant = -1;
+        unordered_map<int, int> mpp;
+        for (int& it : nums) {
+            mpp[it]++;
+        }
+        for(auto&[key,val]:mpp){
+            if(val > (n/2)){
+                dominant = key;
+            }
+        }
+        
+        if(dominant == -1)return -1;
+        int prefixCount = 0,suffixCount = mpp[dominant];
+        for(int i=0;i<n-1;i++){
+            if(nums[i] == dominant){
+                prefixCount++;
+                suffixCount--;
+            }
+            if((prefixCount*2 > i+1) && (suffixCount *2 > n-i-1)) return i;
+        }
+        return -1;
+    }
+};
