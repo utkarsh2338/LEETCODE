e1e61923a547535c2dbe2e31ff21e2e8dc2d85e1
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-04-13 01:46:12 +0530

    Time: 775 ms (100%), Space: 55.2 MB (100%) - LeetHub

--- 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.java
+++ 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.java
@@ -6,7 +6,7 @@ class Solution {
         HashSet<Long>set=new HashSet<>();
         for(int i=0;i<n;i++)for(int j=i;j<n;j++)set.add((long)nums[i]^nums[j]);
             
-        int cnt=0
+        int cnt=0;
         for(long it:set){
             for(int nt:nums){
                 if(vis[(int)it^nt]==0)cnt++;
