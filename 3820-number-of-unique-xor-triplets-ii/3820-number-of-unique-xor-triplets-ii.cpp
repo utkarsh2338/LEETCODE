2d70d692d6aab26c82221353b9e7a308795be2cc
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-04-13 01:39:03 +0530

    Time: 2237 ms (51.43%), Space: 100.2 MB (74.29%) - LeetHub

--- 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.cpp
+++ 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.cpp
@@ -1,29 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
+        unordered_set<int>st,s;
         int n = nums.size();
-        sort(nums.begin(), nums.end());
-
-        unordered_set<long long> xorPairs;
-        for (int i = 0; i < n; ++i)
-            for (int j = i; j < n; ++j)
-                xorPairs.insert((long long)(nums[i]) ^ nums[j]);
-
-        vector<bool> vis((int)1e5, false);
-        int cnt = 0;
-        for (auto it : xorPairs) {
-            for (int nt : nums) {
-                int x = (int)(it ^ nt);
-                if (x < (int)vis.size() && !vis[x]) {
-                    cnt++;
-                    vis[x] = true;
-                }
+        if(n<3) return n;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                st.insert(nums[i]^nums[j]);
             }
         }
-
-        return cnt;
+        for(auto& num:st){
+            for(auto& x: nums){
+                s.insert(num^x);
+            }
+        }
+        return s.size();
     }
 };
