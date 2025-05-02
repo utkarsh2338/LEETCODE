c028fc464a93cc9600f1657ee890d5d00edced58
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-05-02 13:16:48 +0530

    Time: 27 ms (21.79%), Space: 22.1 MB (49.64%) - LeetHub

---
+++ 0868-push-dominoes/0868-push-dominoes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        dominoes = 'L' + dominoes + 'R';
+        string ans;
+        int prev = 0;
+        for(int curr = 1; curr<dominoes.length();curr++){
+            if(dominoes[curr] == '.'){
+                continue;
+            }
+            int span = curr - prev - 1;
+            if(prev>0){
+                ans += dominoes[prev];
+            }
+            if(dominoes[prev] == dominoes[curr]){
+                ans += string(span,dominoes[prev]);
+            }
+            else if(dominoes[prev] == 'L' && dominoes[curr] == 'R'){
+                ans += string(span,'.');
+            }
+            else{
+                ans += string(span/2,'R') + string(span%2,'..') + string(span/2,'L');
+            }
+            prev = curr;
+        }   
+        return ans;
+    }
+};
