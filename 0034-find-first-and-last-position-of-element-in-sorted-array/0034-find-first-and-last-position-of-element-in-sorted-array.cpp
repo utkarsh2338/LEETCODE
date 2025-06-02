edb44115e737ec5b905eed51f7c70683deafb861
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-06-02 13:17:27 +0530

    Time: 0 ms (100%), Space: 17.5 MB (85.98%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,84 +1,44 @@
-// class Solution {
-// public:
-//     vector<int> searchRange(vector<int>& nums, int target) {
-//         int n = nums.size()-1;
-//         int s = 0;
-//         int e = nums.size()-1;
-//         vector<int>ans;
-//         //  binary search to find first position of an element 
-//         while(s<=e){
-//             int mid = s+(e-s)/2;
-//             if(nums[mid] == target){
-//                 if(mid>0 && nums[mid-1] == nums[mid]){
-//                     e = mid-1;
-//                 }
-//                 else{
-//                     ans.push_back(mid);
-//                 }
-//             }
-//             else if(nums[mid] > target){
-//                 s = mid+1;
-//             }
-//             else {
-//                 e = mid-1;
-//             }
-//         }
-//         // binary search to find the second position of the element
-//         while(s<=e){
-//             int mid = s+(e-s)/2;
-//             if(nums[mid]== target){
-//                 if(mid<n-1 && nums[mid]== nums[mid+1]){
-//                     s = mid+1;
-//                 }
-//                 else{
-//                     ans.push_back(mid);
-//                 }
-//             }else if(nums[mid] > target){
-//                 s = mid+1;
-//             }
-//             else {
-//                 e = mid-1;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans(2, -1); // Initialize result as {-1, -1}
-
-        int s = 0, e = nums.size() - 1;
-
-        // Binary search for the first position
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] == target) {
-                ans[0] = mid; // Found the target
-                e = mid - 1; // Move left to find the first occurrence
-            } else if (nums[mid] > target) {
-                e = mid - 1;
-            } else {
-                s = mid + 1;
+    int lb(vector<int>& nums, int target){
+        int n = nums.size();
+        int l = 0,h = n-1;
+        int ans = n;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(nums[mid] >= target){
+                ans = mid;
+                h = mid-1;
+            }
+            else{
+                l = mid+1;
             }
         }
-
-        // Binary search for the last position
-        s = 0;
-        e = nums.size() - 1;
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] == target) {
-                ans[1] = mid; // Found the target
-                s = mid + 1; // Move right to find the last occurrence
-            } else if (nums[mid] > target) {
-                e = mid - 1;
-            } else {
-                s = mid + 1;
-            }
-        }
-
         return ans;
+    }
+    int ub(vector<int>& nums, int target){
+        int n = nums.size();
+        int l = 0,h = n-1;
+        int ans = n;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(nums[mid] > target){
+                ans = mid;
+                h = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = lb(nums, target);
+        int second = ub(nums,target);
+        
+        if (first == nums.size() || nums[first] != target) {
+            return {-1, -1};
+        }
+        return {first,second-1};
     }
 };
