e64f06104113f1b062fe9dbe284685a0a08bd20d
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-06-08 16:37:04 +0530

    Time: 0 ms (100%), Space: 10.6 MB (45.9%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,18 @@
-// Recursion + memoisation approach 
+// now the tabulation appraoch bottom - up approach where we will define the states first and then use the for loop
 class Solution {
 public:
-    int solve(int idx,vector<int>& nums,vector<int>& dp){
-        // base case
-        if(idx >= nums.size()){
-            return 0;
-        }
-        if(dp[idx] != -1) return dp[idx];
-        int steal = nums[idx] + solve(idx+2,nums,dp);
-        int skip = solve(idx+1,nums,dp);
-        return dp[idx] = max(skip,steal);
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1,-1);
-        return solve(0,nums,dp);
+        vector<int>dp(n+1);
+        // dp[i] ka matlab hoga max stolen money till i
+        // isme problem aa rahi thi initial states define karne me toh ham kya karenge upar wali line ka use karenge aur apna array fill karege 
+        dp[0] = 0;// iska matlab max stolen money till zero house ab zero house hai toh 0 money
+        dp[1] = nums[0];// ek house matlab agr ek hi ghar hai toh obv vo array ka pahla hi element hoga
+        for(int i=2;i<=n;i++){
+            int steal = nums[i-1] + dp[i-2];
+            int skip = dp[i-1];
+            dp[i] =  max(steal,skip);
+        }
+        return dp[n];
     }
 };
-// Initially I was using money also as a factor which was forcing me to make 2d dp which is not at all optimized in this case hence after removing money factor i was able to make the recursion and memoization process happen.
