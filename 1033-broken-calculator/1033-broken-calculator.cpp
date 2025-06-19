f260a8c7b89fa7e825e89b07fe808dffa53d01df
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-06-19 22:09:08 +0530

    Time: 0 ms (100%), Space: 7.8 MB (50.15%) - LeetHub

--- 1033-broken-calculator/1033-broken-calculator.cpp
+++ 1033-broken-calculator/1033-broken-calculator.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int brokenCalc(int startValue, int target) {
-        int cnt = 0;
-        while(target != startValue){
-            if(target % 2 == 0 && target>startValue){
+        int result = 0;
+        while(target > startValue)
+        {
+            if(target % 2 == 0)
+            {
                 target /= 2;
-                cnt++;
             }
-            else{
+            else
+            {
                 target += 1;
-                cnt++;
             }
+            result ++;
         }
-        return cnt;
+
+        int ans = result + (startValue - target);
+        return ans;
     }
 };
