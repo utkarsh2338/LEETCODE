c2e33127afbc4e34d74927e625812854750d5766
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-06-21 22:35:23 +0530

    Time: 4 ms (76.45%), Space: 19.5 MB (55.07%) - LeetHub

---
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -0,0 +1,42 @@
+// k special tab hoga jab |freq(word[i]) - freq(word[j])| <= k
+class Solution {
+public:
+    int minimumDeletions(string word, int k) {
+        vector<int>freq(26,0);
+        for(char ch: word){
+            freq[ch-'a']++;
+        }
+        // stored the frequncy of each character
+        vector<int>freqList;
+        freqList.reserve(26);
+        for(int f : freq){
+            if(f>0) freqList.push_back(f);
+        }
+        
+        sort(begin(freqList), end(freqList));
+
+        int n = freqList.size(), total = word.size(), removedLeft = 0,
+            inWindowSum = 0, best = INT_MAX, r = 0;
+
+        for (int l = 0; l < n; ++l) {
+            int base = freqList[l];
+            int limit = base + k;
+
+            while (r < n && freqList[r] <= limit)
+                inWindowSum += freqList[r++];
+
+            int rightCount = n - r;
+            int rightSum = total - inWindowSum;
+            int delRight = rightSum - rightCount * limit;
+
+            best = min(best, removedLeft + delRight);
+
+            total -= freqList[l];
+            removedLeft += freqList[l];
+            inWindowSum -= freqList[l];
+        }
+
+        return best;
+        
+    }
+};
