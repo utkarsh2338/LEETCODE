f83738b9e65497a203f61c9d07ce9e26dfa04951
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-06-27 08:45:23 +0530

    Time: 6 ms (37.05%), Space: 33.1 MB (60.9%) - LeetHub

---
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(head == NULL ) return NULL;
+        if(head->next == NULL){
+            return new TreeNode(head->val);
+        }
+        ListNode* fast = head;
+        ListNode* slow = head;
+        ListNode* slowPrev = head;
+        while(fast && fast->next){
+            slowPrev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        TreeNode* root = new TreeNode(slow->val);
+        slowPrev -> next = NULL;
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(slow->next);
+        return root;
+    }
+};
