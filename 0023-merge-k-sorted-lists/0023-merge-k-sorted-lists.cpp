ee7fa3aef3861de818df3e416d64a8e4fef64c26
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-06-28 00:20:23 +0530

    Time: 192 ms (5.69%), Space: 18.6 MB (51.54%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,34 +1,30 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* merge2Lists(ListNode* head1, ListNode*head2){
-        if(head1 == NULL && head2 == NULL) return NULL;
-        if(head1 == NULL) return head2;
-        if(head2 == NULL) return head1;
-        if(head1-> val <= head2->val){
-            head1->next = merge2Lists(head1->next,head2);
+    ListNode* merge2Lists(ListNode* head1, ListNode* head2) {
+        if (head1 == NULL)
+            return head2;
+        if (head2 == NULL)
             return head1;
-        }
-        else{
-            head2->next = merge2Lists(head1,head2->next);
+        if (head1->val <= head2->val) {
+            head1->next = merge2Lists(
+                head1->next, head2); // head1 ke next ke baad jo linked list hai
+                                     // usko lena hai aur apna head2 toh hai hi.
+            return head1;
+        } else {
+            head2->next = merge2Lists(
+                head1, head2->next); // head2 ke next ke baad me jo linked list
+                                     // hai usko lena hai aur fir head1 toh hai
+                                     // hi starting se mere pas.
             return head2;
         }
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int n = lists.size();
-        if(n == 0) return NULL;// important line for avoiding runtime error
+        if (n == 0)
+            return NULL; // important line for avoiding runtime error
         ListNode* merged = lists[0];
-        for(int i=1;i<n;i++){
-            merged = merge2Lists(merged,lists[i]);
+        for (int i = 1; i < n; i++) {
+            merged = merge2Lists(merged, lists[i]);
         }
         return merged;
     }
