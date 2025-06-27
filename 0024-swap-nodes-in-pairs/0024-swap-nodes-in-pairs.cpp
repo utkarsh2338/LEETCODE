a8c4b1687ab4e801437fd103a8fd78beccf4349a
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-06-28 03:04:02 +0530

    Time: 0 ms (100%), Space: 11.2 MB (19.42%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -1,35 +1,13 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// Recursive Solution
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if(head == NULL) return NULL;
-        vector<int>arr;
-        ListNode* temp = head;
-        int n = 1;
-        while(temp != NULL){
-            arr.push_back(temp->val);
-            temp = temp->next;
-            n+=1;
-        }
-        for(int i=0;i<arr.size()-1;i+=2){
-            swap(arr[i],arr[i+1]);
-        }
-        ListNode* h = new ListNode(arr[0]);
+        if(head == NULL || head->next == NULL) return head;
+
+        ListNode* temp = head->next;
+        head -> next = swapPairs(head->next->next);
+        temp->next = head;
         
-        ListNode* curr = h;
-        for(int i=1;i<arr.size();i++){
-            curr->next = new ListNode(arr[i]);
-            curr = curr->next;
-        }
-        return h;
+        return temp;
     }
 };
