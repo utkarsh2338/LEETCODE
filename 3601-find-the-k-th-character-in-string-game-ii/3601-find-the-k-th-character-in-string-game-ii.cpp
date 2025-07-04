55bc0c4a42716e95a757f2a1b22aaecdfc7f73ad
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-07-04 13:36:12 +0530

    Time: 0 ms (100%), Space: 28.7 MB (14.35%) - LeetHub

--- 3601-find-the-k-th-character-in-string-game-ii/3601-find-the-k-th-character-in-string-game-ii.cpp
+++ 3601-find-the-k-th-character-in-string-game-ii/3601-find-the-k-th-character-in-string-game-ii.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
-    char kthCharacter(long long k, vector<int>& arr) {
-        int c = 0; 
-        k--;
-        for (int i = 0; k != 0; i++, k >>= 1) {
-            c += ((k & 1) & arr[i]);
+    char kthCharacter(long long k, vector<int>& operations) {
+        int shift = 0;
+        vector<long long> lengths;
+        long long len = 1;
+
+        for (int op : operations) {
+            len *= 2;
+            lengths.push_back(len);
+            if (len >= k) break;
         }
-        return (char)((c % 26) + 'a');
+
+        for (int i = lengths.size() - 1; i >= 0; --i) {
+            long long half = lengths[i] / 2;
+            int op = operations[i];
+            if (k > half) {
+                k -= half;
+                if (op == 1) shift++;
+            }
+        }
+
+        return (char)((('a' - 'a' + shift) % 26) + 'a');
     }
 };
