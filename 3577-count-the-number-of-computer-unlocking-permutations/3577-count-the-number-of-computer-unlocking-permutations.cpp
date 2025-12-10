80d44e886389455dd21aec87612eeb83e016fa07
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-12-10 17:08:02 +0530

    Time: 0 ms (100%), Space: 92.9 MB (23.5%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
@@ -2,10 +2,6 @@ using ll = long long;
 const int MOD = 1e9+7;
 class Solution {
 public:
-    ll factorial(ll n){
-        if(n<=1) return 1;
-        return n*factorial(n-1); 
-    }
     int countPermutations(vector<int>& complexity) {
         int n = complexity.size();
         int root = complexity[0];
