7f5486950b4b46a9eb687e140bf02195da0437ab
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2026-03-17 23:59:03 +0530

    Time: 0 ms (100%), Space: 8.4 MB (86.95%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int ans = 0 ; 
-        for( int i = 1 ; i <= n; i++ ) {
-            int d=to_string(i).size() ; 
-            ans += (d-1)/3; 
-        }
-        return ans ; 
+        return max(0,n-999);
     }
 };
