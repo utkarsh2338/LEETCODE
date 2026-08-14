5bdf1c97c37c6baa22a71b2806d88a7a0d6c1de0
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2026-08-15 02:46:15 +0530

    Time: 0 ms (100%), Space: 10 MB (11.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        if(s.length() != t.length()) return false;
+        unordered_map<char,int>mpp1,mpp2;
+        for(int i=0;i<s.length();i++){
+            mpp1[s[i]]++;
+            mpp2[t[i]]++;
+        }
+        return mpp1 == mpp2;
     }
 };
