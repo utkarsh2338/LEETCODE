a21bed0c8eda1879d859790e6ef4809161d4d1ea
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2026-08-20 22:34:00 +0530

    Time: 0 ms (100%), Space: 12.6 MB (14.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int midx = m - 1;
-        int nidx = n - 1;
-        int right = m + n - 1;
-
-        while (nidx >= 0) {
-            if (midx >= 0 && nums1[midx] > nums2[nidx]) {
+        int midx = m-1,nidx = n-1,right = m+n-1;
+        while(nidx >= 0){
+            if(midx>=0 && nums1[midx]>nums2[nidx]){
                 nums1[right] = nums1[midx];
                 midx--;
-            } else {
+            }
+            else{
                 nums1[right] = nums2[nidx];
                 nidx--;
             }
             right--;
-        }        
+        }
     }
 };
